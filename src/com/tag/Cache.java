dddd2652a3f7f41f0628eefe19268b2355e099b3
tagadvance@gmail.com <tagadvance@gmail.com@d7de2c5c-bb2f-6289-aa25-c38c7530a4a7>
2011-09-29 18:31:10 +0000

    Cache.java

--- src/com/tag/Cache.java
+++ src/com/tag/Cache.java
@@ -29,42 +29,95 @@
 
 package com.tag;
 
+import java.util.Collection;
 import java.util.Map;
+import java.util.Set;
 
+import com.google.common.base.Function;
+import com.google.common.collect.MapEvictionListener;
 import com.google.common.collect.MapMaker;
 
-public abstract class Cache<K, V> {
+// TODO: refactor this to use CacheBuilder
+// http://guava-libraries.googlecode.com/svn/trunk/javadoc/com/google/common/cache/CacheBuilder.html
+public abstract class Cache<K, V> implements Function<K, V>,
+	MapEvictionListener<K, V>, Map<K, V> {
 
-    private Map<K, V> map;
+    protected final Map<K, V> map;
 
     public Cache() {
 	this.map = createMap();
     }
 
     protected Map<K, V> createMap() {
-	return new MapMaker().softValues().makeMap();
+	return new MapMaker().softValues().evictionListener(this)
+		.makeComputingMap(this);
     }
 
+    @Override
+    public abstract V apply(K key);
+
+    @Override
+    public void onEviction(K key, V value) {
+    }
+
+    @Override
     public int size() {
 	return map.size();
     }
 
+    @Override
     public boolean isEmpty() {
 	return map.isEmpty();
     }
 
-    public V get(K key) {
-	if (!map.containsKey(key)) {
-	    V value = create(key);
-	    map.put(key, value);
-	}
+    @Override
+    public boolean containsKey(Object key) {
+	return map.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+	return map.containsValue(value);
+    }
+
+    @Override
+    public V get(Object key) {
 	return map.get(key);
     }
 
-    protected abstract V create(K key);
+    @Override
+    public V put(K key, V value) {
+	return map.put(key, value);
+    }
 
+    @Override
+    public V remove(Object key) {
+	return map.remove(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+	map.putAll(m);
+    }
+
+    @Override
     public void clear() {
 	map.clear();
+    }
+
+    @Override
+    public Set<K> keySet() {
+	return map.keySet();
+    }
+
+    @Override
+    public Collection<V> values() {
+	return map.values();
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+	return map.entrySet();
     }
 
 }
